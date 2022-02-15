b5d63bf5274bd24c81cceb3960d1d2694365c84e
Xprogrammer777 <76904094+Xprogrammer777@users.noreply.github.com>
2022-02-15 16:49:56 +0100

    Update linuxinstall.sh

--- linuxinstall.sh
+++ linuxinstall.sh
@@ -3,7 +3,7 @@ sleep(2)
 echo '[*]INSTALLING REQUIREMENTS'
 chmod +x bypassworddiscord.py
 pip install -r requirements.txt
-sleep(2)
+sleep 2
 echo '[*]FINISHED'
 echo 'Do you want to run the program? y/n'
 read launch
@@ -14,11 +14,11 @@ python3 bypassworddiscord.py
 elif $launch ==  'n'
 then
 echo '[!] exit....'
-sleep(2)
+sleep 2
 exit
 else
 echo 'Check your syntax'
-sleep(2)
+sleep 2
 exit
 fi
 
