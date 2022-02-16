aa750ed03757f6c5fecccbbdbffe9807c9d607ba
Xprogrammer777 <76904094+Xprogrammer777@users.noreply.github.com>
2022-02-16 09:22:54 +0100

    Update linuxinstall.sh

--- linuxinstall.sh
+++ linuxinstall.sh
@@ -1,23 +1,23 @@
-echo 'WELCOME TO THE LINUX INSTALLER'
+echo 'LINUX INSTALL'
 sleep 2
-echo '[*]INSTALLING REQUIREMENTS'
+echo '[*] Installing requirements...'
 chmod +x bypassworddiscord.py
 pip install -r requirements.txt
 sleep 2
-echo '[*]FINISHED'
+echo '[*] Installation completed'
 echo 'Do you want to run the program? y/n'
 read launch
 if $launch == "y"
 then
-echo '[*] Running'
+echo '[*] Running...'
 python3 bypassworddiscord.py
 elif $launch ==  'n'
 then
-echo '[!] exit....'
+echo '[!] Exit....'
 sleep 2
 exit
 else
-echo 'Check your syntax'
+echo 'Check your syntax!'
 sleep 2
 exit
 fi
