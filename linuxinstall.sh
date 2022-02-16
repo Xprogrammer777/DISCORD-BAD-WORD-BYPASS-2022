39f237ce1660fee31c500d8bfef184788c09cd84
Xprogrammer777 <76904094+Xprogrammer777@users.noreply.github.com>
2022-02-16 09:31:16 +0100

    Update linuxinstall.sh

--- linuxinstall.sh
+++ linuxinstall.sh
@@ -7,11 +7,11 @@ sleep 2
 echo '[*] Installation completed'
 echo 'Do you want to run the program? y/n'
 read launch
-if $launch == "y"
+if [ $launch == "y" ]
 then
 echo '[*] Running...'
 python3 bypassworddiscord.py
-elif $launch ==  'n'
+elif [ $launch ==  'n' ]
 then
 echo '[!] Exit....'
 sleep 2
