cbf85868d0782a54430e559c425ec4df75269b40
Xprogrammer777 <76904094+Xprogrammer777@users.noreply.github.com>
2022-02-19 15:45:07 +0400

    Create fixing_issue_test.py

---
+++ fixing_issue_test.py
@@ -0,0 +1,59 @@
+#some problem with the other script I'm fixing a issue so here is a "test"
+
+import os
+import sys
+import time
+import pyfiglet
+
+word = ""
+banner = pyfiglet.figlet_format("DISCORD BLACKLISTED WORDS BYPASS 2022")
+
+print(banner)
+print("Fast, simple EDUCATIONAL PURPOSE ONLY")
+word = str(input("[*] Type the word you want to bypass: "))
+
+def startprocess():
+
+    if "a" in word:
+        bypassed = word.replace('a', '\u0430') 
+
+    if "c" in word:
+        bypassed = word.replace('c', '\u03F2')
+
+    if "e" in word:
+        bypassed = word.replace('e', '\u0435')
+
+    if "o" in word:
+        bypassed = word.replace('o', '\u043E')
+
+    if "p" in word:
+        bypassed = word.replace('p', '\u0440')
+
+    if "s" in word:
+        bypassed = word.replace('s', '\u0455')
+
+    if "d" in word:
+        bypassed = word.replace('d', '\u0501')
+    
+    if "q" in word:
+        bypassed = word.replace('q', '\u051B')
+
+    if "w" in word:
+        bypassed = word.replace('w', '\u051D')
+    #looking the code? I will add other IDN, but not homograph, because some words are not "bypassed"
+
+    #print the result
+    
+    print("[*] Porcess succefully finished!")
+    
+    time.sleep(1)
+
+    print("[*] COPY AND PASTE THIS WORD: ", bypassed)
+
+if word == "":
+    time.sleep(1)
+    print("[!] Please type a word")
+else:
+    print("[*] Starting...")
+    time.sleep(1)
+    startprocess()
